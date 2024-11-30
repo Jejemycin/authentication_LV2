347f90df3f144706252b936e0f736a1b095b7760
Taiye Jeje <jej4real@yahoo.com>
2024-11-30 14:37:35 -0600

    incorporate bcrypt for password hashing

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import bcrypt from "bcrypt";
+
 
 const app = express();
 const port = 3000;
+const saltRounds = 10;
 
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
   database: "secrets",
-  password: "123456",
+  password: "Swordfish@1984",
   port: 5432,
 });
 db.connect();
@@ -41,12 +44,19 @@ app.post("/register", async (req, res) => {
     if (checkResult.rows.length > 0) {
       res.send("Email already exists. Try logging in.");
     } else {
-      const result = await db.query(
-        "INSERT INTO users (email, password) VALUES ($1, $2)",
-        [email, password]
-      );
-      console.log(result);
-      res.render("secrets.ejs");
+      // password Hashing
+      bcrypt.hash(password, saltRounds, async (err, hash) =>{
+        if (err){
+          console.error("Error hashing password:", err);
+        } else {
+        const result = await db.query(
+          "INSERT INTO users (email, password) VALUES ($1, $2)",
+          [email, hash]
+        )
+        console.log(result);
+        res.render("secrets.ejs");
+      };
+      })
     }
   } catch (err) {
     console.log(err);
